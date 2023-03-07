305ee3600669e7841843b4d33f6ea5d5a603f7f7
Hugo Moran <moranh56@gmail.com>
2023-03-06 19:55:29 -0600

    Add paging extensions

---
+++ Data/Extensions/PagingExtensions.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using ProductsApp.Models;
+
+namespace ProductsApp.Data.Extensions;
+
+public static class PagingExtension {
+    public static async Task<DataCollection<T>> GetPagedAsync<T>(this IQueryable<T> query, int page, int take)
+    {
+        var offset = page - 1 > 0 ? page * take : 0;
+
+        var result = new DataCollection<T>
+        {
+            Items = await query.Skip(offset).Take(take).ToListAsync(),
+            Total = await query.CountAsync(),
+            Page = page
+        };
+
+        if (result.Total > 0)
+        {
+            result.Pages = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(result.Total) / take));
+        }
+
+        return result;
+    }
+}
