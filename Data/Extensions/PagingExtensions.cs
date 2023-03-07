53d0bc57185667ee596aabc90751e6888ec95c0c
Hugo Moran <moranh56@gmail.com>
2023-03-06 23:46:53 -0600

    Fix paging offset

--- Data/Extensions/PagingExtensions.cs
+++ Data/Extensions/PagingExtensions.cs
@@ -6,7 +6,7 @@ namespace ProductsApp.Data.Extensions;
 public static class PagingExtension {
     public static async Task<DataCollection<T>> GetPagedAsync<T>(this IQueryable<T> query, int page, int take)
     {
-        var offset = page - 1 > 0 ? page * take : 0;
+        var offset = page - 1 > 0 ? (page-1) * take : 0;
 
         var result = new DataCollection<T>
         {
